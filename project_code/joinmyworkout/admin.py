b1a7f901619f7f795426f6eb47078dfbf5cbcbbe
travisbartholome <travis.bartholome@gmail.com>
2016-10-15 17:36:07 -0500

    Working on merge conflict.

--- project_code/joinmyworkout/admin.py
+++ project_code/joinmyworkout/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Event_Type)
+admin.site.register(Event_Class)
+admin.site.register(Event_Type_To_Class)
+admin.site.register(Event_Class_To_Experience_Level)
+admin.site.register(Workout_Event)
+admin.site.register(Event_To_User)
