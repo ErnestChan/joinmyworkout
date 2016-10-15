7a526bb74625a1631e4032c9aac4fb0a3904d749
jhnguy <jhnguye0@gmail.com>
2016-10-15 18:15:56 -0500

    Made some changes.

--- project_code/joinmyworkout/urls.py
+++ project_code/joinmyworkout/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^create$', views.create_event, name='createevent'),
+    url(r'^create_event$', views.create_event, name='create_event'),
 ]
