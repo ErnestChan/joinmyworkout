56882271febafbe42b79bd672b8c101c8885dfdd
ErnestChan <ekc1@rice.edu>
2016-10-15 19:44:10 -0500

    modified url

--- project_code/mysite/urls.py
+++ project_code/mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('joinmyworkout.urls')),
     url('', include('social.apps.django_app.urls', namespace='social')),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
 ]
